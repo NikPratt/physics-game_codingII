c974d085fce9beb1dfae306aae6c50ce533bcd86
NikPratt <nraj5@pratt.edu>
2026-03-26 15:42:15 -0400

    Grab and throw

--- physics game_codingII/Assets/InteractableObject.cs
+++ physics game_codingII/Assets/InteractableObject.cs
@@ -1,42 +1,47 @@
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
-    public Color highlightColor = new Color(r: 1f, g:95, b:.6f)
-
+    public Color highlightedColor = new Color(r: 1f, g: 95, b: .6f);
     [Range(0f, 1f)] public float highlightStrength = .4f;
 
-    private RenderBuffer objectRenderer;
+    private Renderer objectRenderer;
+
     private Color originalColor;
+
     private bool isHighlighted = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Awake()
     {
         objectRenderer = GetComponent<Renderer>();
         if (objectRenderer != null)
+        {
+            originalColor = objectRenderer.sharedMaterial.color;
+        }
 
-       originalColor = object.render.shadedMaterial.color;
+        else
+        {
+            Debug.Log($"Interactable object on {gameObject.name} has no renderer. highlight worn work");
+        }
     }
-    else
-    {
-        Debug.Log
-    }
-    
     public void Highlight()
     {
-        Debug.Log("no obj render & ishighlighted is true");
-        return;
+        if (isHighlighted || objectRenderer == null)
+        {
+            Debug.Log("no rednerer");
+
+        }
+        objectRenderer.material.color = Color.Lerp(originalColor, highlightedColor, highlightStrength);
+
+        isHighlighted = true;
     }
-    // Update is called once per frame
-    objectRenderer.material.color = Color.Lerp(originalColor, highlightColor, highlightStrength);
-    isHighlighted = true;
-}
 
 
-public void Unhighlight()
-{
-    if (!isHighlighted || objectRender == null) return;
-    objectRender.material.color = originalColor;
-    isHighlited = false;
-}
+    public void Unhighlight()
+    {
+        if (!isHighlighted && objectRenderer == null) return;
+        objectRenderer.material.color = originalColor;
+        isHighlighted = false;
+    }
+}   
