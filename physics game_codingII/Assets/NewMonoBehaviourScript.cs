32358ab3cd9f83b4dc999164073127b4dbe0e634
NikPratt <nraj5@pratt.edu>
2026-03-26 13:41:10 -0400

    3-26

---
+++ physics game_codingII/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //called every fixed updae while holding an object 
+    //smoothly moves the rigidbody toward the hold point 
+    
+    void MoveHeldObject()
+    {
+        Vector3 targetPos = cameraTransform.position + cameraTransform.forward * holdDistance;
+        Vector3 currentPos = heldObject.position;
+        Vector3 newPos = Vector3.Lerp(currentPos, targetPos, holdSmoothing * Time.fixedDeltaTime);
+        heldObject.MovePosition(newPos);
+    }
+    void TryGrab()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitl
+
+        Debug.DrawRay(transform.position, transform.forward * 5, Color.red, .5f);
+
+        if(Physics.Raycast(ray, out hit, grabRAnge))
+        {
+            InteractableObject interactable = hit.collider.GetComponent<InteractableObject>();
+            if(interactable != null)
+        }
+            heldObject.useGravity = false;
+            heldObject.freezeRotation = true;
+
+            Vector3 newPos = Vector3.Lerp(currentPos, targetPos, holdSmoothing * Time.fixedDeltaTime);
+
+            heldObject.MovePosition(newPos);
+    }    
+
+    void ThrowObject()
+    {
+        if (HeldObject == null); 
+        MoveHeldObject 
+    }
+}
